ce7390fa052cfcb38eb586610b464f00ad0ffc6b
Gustavo D. Assunção <145854208+gustavo19972023@users.noreply.github.com>
2023-12-23 13:41:53 -0300

    go

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css'
+import emojipedia from './emojipedia';
+import Entry from './Entry';
+
+function createEntry(entry){
+  return <Entry 
+  key={entry.id}
+  emoji={entry.emoji}
+  name={entry.name}
+  meaning={entry.meaning}
+  />
+}
+
+function App() {
+
+  return (
+    <div>
+      <h1>
+        <span>Different Accents and Sayings among English Speaking Countries</span>
+      </h1>
+      <div className='container'>
+      {emojipedia.map(createEntry)}
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "🗽",
+      name: "The U.S",
+      meaning:
+        "“That was DOPE!” or “That's a bummer!” Or even the expression once in a blue moon"
+    },
+    {
+      id: 2,
+      emoji: "🦘",
+      name: "Australia",
+      meaning:
+        "If you're an Australian, you can call yourself an Aussie."
+    },
+    {
+      id: 3,
+      emoji: "💂",
+      name: "England",
+      meaning:
+        "Bloke for English people means a male, the same as guy or dude"
+    },
+    {
+        id: 4,
+        emoji: "🍁",
+        name: "Canada",
+        meaning:
+          "Timmies is the first in the list of popular Canadian slang, short for Tim Hortons, Canada's iconic coffee and doughnut chain."
+      },
+      {
+        id: 5,
+        emoji: "☘️",
+        name: "Ireland",
+        meaning:
+          "The word ‘Craic’ can be used in a couple of different ways. The first is its use to describe something as ‘Fun’, for example, ‘Last night was some craic!’. "
+      },
+      {
+        id: 6,
+        emoji: "🥝",
+        name: "England",
+        meaning:
+          "Hard case: Funny. “That Jack, he’s a hard case.”"
+      }
+  ];
+  
+  export default emojipedia;
